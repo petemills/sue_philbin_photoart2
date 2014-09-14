01d1471ba71f2fcb57bc1664404f6cbb7e4012c7
Pete <pete@pete.com>
2014-09-14 15:09:13 +1200

    Initial commit

--- js/app.js
+++ js/app.js
@@ -14,6 +14,21 @@ app.controller("GalleryController", function(){
 app.controller("ImageController", function(){
 	this.currentImage = 1;
 
+	this.numImagePairs = {
+		1: "static/images/duck1.jpg",
+		2: "static/images/duck1.jpg",
+		3: "static/images/duck1.jpg",
+		4: "static/images/duck1.jpg",
+		5: "static/images/duck1.jpg",
+		6: "static/images/duck1.jpg",
+		7: "static/images/duck1.jpg",
+		8: "static/images/duck1.jpg",
+		9: "static/images/duck1.jpg",
+		10: "static/images/duck1.jpg",
+		11: "static/images/duck1.jpg",
+		12: "static/images/duck1.jpg",
+	};
+
 	this.selectImage = function(setImage) {
 		this.currentImage = setImage;
 	};
