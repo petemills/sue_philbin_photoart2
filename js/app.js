6b172937026e9a658397c8d39ad937a3adcfc02c
Jared Newman <jnewman@jnewman-mbp.dyn.yojoe.local>
2014-09-14 14:49:33 +1000

    Fix up gallery to use ng-repeat

--- js/app.js
+++ js/app.js
@@ -12,44 +12,32 @@ app.controller("GalleryController", function(){
 });
 
 app.controller("ImageController", function(){
-	this.currentImage = 1;
+	this.currentImageIndex = 0;
 
 	// The numbers will match up between images and thumbnails eventually
 	// i.e 1: "static/images/cow1.jpg",
 	// 	   1: "static/images/thumbnails/cow1_thumb.jpg",
-	this.numImagePairs = {
-		1: "static/images/duck1.jpg",
-		2: "static/images/duck1.jpg",
-		3: "static/images/duck1.jpg",
-		4: "static/images/duck1.jpg",
-		5: "static/images/duck1.jpg",
-		6: "static/images/duck1.jpg",
-		7: "static/images/duck1.jpg",
-		8: "static/images/duck1.jpg",
-		9: "static/images/duck1.jpg",
-		10: "static/images/duck1.jpg",
-		11: "static/images/duck1.jpg",
-		12: "static/images/duck1.jpg",
-	};
-	this.numThumbPairs = {
-		1: "static/images/thumbnails/mountains1_thumb.jpg",
-		2: "static/images/thumbnails/mountains1_thumb.jpg",
-		3: "static/images/thumbnails/mountains1_thumb.jpg",
-		4: "static/images/thumbnails/mountains1_thumb.jpg",
-		5: "static/images/thumbnails/mountains1_thumb.jpg",
-		6: "static/images/thumbnails/mountains1_thumb.jpg",
-		7: "static/images/thumbnails/mountains1_thumb.jpg",
-		8: "static/images/thumbnails/mountains1_thumb.jpg",
-		9: "static/images/thumbnails/mountains1_thumb.jpg",
-		10: "static/images/thumbnails/mountains1_thumb.jpg",
-		11: "static/images/thumbnails/mountains1_thumb.jpg",
-		12: "static/images/thumbnails/mountains1_thumb.jpg",
-	};
+	this.images = [
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"},
+		{src: "static/images/duck1.jpg", thumbnail: "static/images/thumbnails/mountains1_thumb.jpg"}
+	];
 
-	this.selectImage = function(setImage) {
-		this.currentImage = setImage;
+	this.selectImage = function(imageIndex) {
+		console.log(imageIndex);
+		this.currentImageIndex = imageIndex;
 	};
-	this.isSelected = function(imageNum) {
-		return this.currentImage === imageNum;
+	this.isSelected = function(imageIndex) {
+		console.log("iss " + imageIndex);
+		return this.currentImageIndex === imageIndex;
 	};
 });
