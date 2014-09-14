5080c8932219c3ef67da50c9de33b7b5fd1c1cfe
Pete <pete@pete.com>
2014-09-14 16:10:03 +1200

    Minor changes

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@ app.controller("GalleryController", function(){
 app.controller("ImageController", function(){
 	this.currentImage = 1;
 
+	// The numbers will match up between images and thumbnails eventually
+	// i.e 1: "static/images/cow1.jpg",
+	// 	   1: "static/images/thumbnails/cow1_thumb.jpg",
 	this.numImagePairs = {
 		1: "static/images/duck1.jpg",
 		2: "static/images/duck1.jpg",
@@ -27,6 +30,20 @@ app.controller("ImageController", function(){
 		10: "static/images/duck1.jpg",
 		11: "static/images/duck1.jpg",
 		12: "static/images/duck1.jpg",
+	};
+	this.numThumbPairs = {
+		1: "static/images/thumbnails/mountains1_thumb.jpg",
+		2: "static/images/thumbnails/mountains1_thumb.jpg",
+		3: "static/images/thumbnails/mountains1_thumb.jpg",
+		4: "static/images/thumbnails/mountains1_thumb.jpg",
+		5: "static/images/thumbnails/mountains1_thumb.jpg",
+		6: "static/images/thumbnails/mountains1_thumb.jpg",
+		7: "static/images/thumbnails/mountains1_thumb.jpg",
+		8: "static/images/thumbnails/mountains1_thumb.jpg",
+		9: "static/images/thumbnails/mountains1_thumb.jpg",
+		10: "static/images/thumbnails/mountains1_thumb.jpg",
+		11: "static/images/thumbnails/mountains1_thumb.jpg",
+		12: "static/images/thumbnails/mountains1_thumb.jpg",
 	};
 
 	this.selectImage = function(setImage) {
